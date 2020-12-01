d93a124fe867126444b150e161ae5578c976a193
zs <baloghzsof@gmail.com>
2020-12-01 14:27:57 +0100

    tweak bird lady examples

--- examples/bird_lady/main_3p.cpp
+++ examples/bird_lady/main_3p.cpp
@@ -19,5 +19,9 @@ auto main() -> int {
   std::cout << "learning " << num_of_learn_games << " games took " << duration << " milliseconds" << std::endl;
 
   constexpr int num_of_play_games = 1000;
+  for (const auto player_id : game.player_ids()) {
+    engine.play_against_random_opponents(player_id, num_of_play_games);
+    engine.play_against_greedy_opponents(player_id, num_of_play_games);
+  }
   engine.play_according_to_policy(num_of_play_games);
 }
